b77d2adf91a8a8dd62ebeb0b286d4d28104dc17b
Geet <geet1998patel@gmail.com>
2022-08-10 20:47:33 +0530

    Heapsort

--- src/chp6/Heap.java
+++ src/chp6/Heap.java
@@ -1,44 +1,60 @@
 package chp6;
 
+import java.util.Arrays;
 import util.CLRSUtil;
 
 public class Heap {
 	private static final CLRSUtil<Integer> clrsUtil = new CLRSUtil<>();
 
 	public static void main(String[] args) {
-		Integer[] A = {4, 1, 3, 2, 16, 9, 10, 14, 8, 7};
+		Integer[] A = {4, 1, 3, 2, 16, 9, 10, 14, 8, 7, 2};
+
+		Integer[] A_ = Arrays.copyOf(A, A.length);
+		System.out.println("A : " + clrsUtil.ArrayToString(A_));
+		buildMaxHeap(A_);
+		System.out.println("A : " + clrsUtil.ArrayToString(A_));
 
 		System.out.println("A : " + clrsUtil.ArrayToString(A));
-		buildMaxHeap(A);
+		heapsort(A);
 		System.out.println("A : " + clrsUtil.ArrayToString(A));
+	}
+
+	private static void heapsort(Integer[] A) {
+		buildMaxHeap(A);
+		int elemCount = 1;
+		for (int i = A.length - 1; i >= 0; i--) {
+			swap(A, 0, i);
+			maxHeapify(A, 0, A.length - elemCount);
+			elemCount++;
+		}
 	}
 
 	private static void buildMaxHeap(Integer[] A) {
 		for (int i = (A.length / 2) - 1; i >= 0; i--) {
-			System.out.printf("i : %d, A[i] : %d\n", i, A[i]);
-			maxHeapify(A, i);
+			maxHeapify(A, i, A.length);
 		}
 	}
 
-	private static void maxHeapify(Integer[] A, int i) {
+	private static void maxHeapify(Integer[] A, int i, int heapSize) {
 		int l = left(i);
 		int r = right(i);
 
-		System.out.println("\tA : " + clrsUtil.ArrayToString(A));
-		System.out.printf("\tComparing %d, %d, %d", i, l, r);
+		System.out.printf("\tComparing %d, %d, %d for heapSize %d\n", i, l, r, heapSize);
 		int largest = i;
-		if (l < A.length && A[l] > A[i]) largest = l;
-		if (r < A.length && A[r] > A[largest]) largest = r;
-		System.out.printf(" : %d\n", largest);
+		if (l < heapSize && A[l] > A[i]) largest = l;
+		if (r < heapSize && A[r] > A[largest]) largest = r;
 
 		if (largest != i) {
 			System.out.printf("\tSwapping %d and %d\n", i, largest);
-			int temp = A[i];
-			A[i] = A[largest];
-			A[largest] = temp;
-			System.out.println("\tA : " + clrsUtil.ArrayToString(A));
-			maxHeapify(A, largest);
+			swap(A, i, largest);
+			maxHeapify(A, largest, heapSize);
 		}
+	}
+
+	private static void swap(Integer[] A, int i, int largest) {
+		int temp = A[i];
+		A[i] = A[largest];
+		A[largest] = temp;
 	}
 
 	private static int right(int i) {
